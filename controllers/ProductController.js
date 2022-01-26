285cce78a1b83f2b6688487cf202fdec8b5b1781
Davi Sousa <davi.sousa.fe@gmail.com>
2022-01-26 00:07:25 -0300

    CRUD with Dynamo

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,7 +7,20 @@ router.get('/', async (req, res) => {
 
     try {
         result = await dynamodb.getAll();
-        res.json(result.Items);
+        if (!result.Items.length > 0) res.sendStatus(404);
+        else res.json(result.Items);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    let id = parseInt(req.params.id);
+    try {
+        result = await dynamodb.getById(id);
+        if(!result.Item) res.sendStatus(404); 
+        else res.json(result);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
@@ -20,12 +33,36 @@ router.post('/', async (req, res) => {
     const product = {id: Date.now() ,name, description, price};
     
     try {
-        result = await dynamodb.addProduct(product);
+        result = await dynamodb.addOne(product);
         res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    let id = parseInt(req.params.id);
+    try {
+        let result = await dynamodb.deleteById(id);
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    let {name, description, price} = req.body;
+    let product = {name, description, price};
+    try {
+        let id = parseInt(req.params.id);
+        await dynamodb.editById(id,product)
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
 
 module.exports = router;
--- services/Product.js
+++ services/Product.js
@@ -13,13 +13,58 @@ class Product{
         }
     }
 
-    addProduct = async(product) =>{
+    getById = async(id) =>{
+        let params = {
+            TableName: 'products',
+            Key:{id}
+        }
+        try {
+            return await dynamoClient.get(params).promise();
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    addOne = async(product) =>{
         let params = {
             TableName: 'products',
             Item: product
         };
         try {
             return await dynamoClient.put(params).promise();
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    deleteById = async(id) =>{
+        let params = {
+            TableName: 'products',
+            Key:{id}
+        }
+        try {
+            return await dynamoClient.delete(params).promise();
+        } catch (error) {
+            throw Error(error);
+        }
+    }
+
+    editById = async(id, product) =>{
+        let params = {
+            TableName: 'products',
+            Key:{id},
+            UpdateExpression: `set #TheName = :x, description = :y, price = :z`,
+            ExpressionAttributeNames: {
+                '#TheName':'name'
+            },
+            ExpressionAttributeValues: {
+                ':x': `${product.name}`,
+                ':y': `${product.description}`,
+                ':z': `${product.price}`
+            }
+        }
+        try {
+            return await dynamoClient.update(params).promise();
         } catch (error) {
             throw Error(error);
         }
